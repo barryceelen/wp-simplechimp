19062ee61e9a337b5695020164c3b2bfead0d349
Barry Ceelen <barry@multipop.org>
2016-10-17 20:16:27 +0200

    Move and update template

--- templates/subscription-form.php
+++ templates/subscription-form.php
@@ -10,6 +10,6 @@
 	<span class="simplechimp-feedback<?php echo $class ?>"<?php echo $style; ?>>
 		<?php echo $message; ?>
 	</span>
-	<input type="email" name="simplechimp_email" value="<?php echo $value; ?>" placeholder="<?php echo self::$options['labels']['placeholder']; ?>">
-	<button name="simplechimp_submit" type="submit" /><?php echo self::$options['labels']['submit']; ?></button>
+	<input type="email" name="simplechimp_email" value="<?php echo $value; ?>" placeholder="<?php echo $labels['placeholder']; ?>">
+	<button name="simplechimp_submit" type="submit" /><?php echo $labels['submit']; ?></button>
 </form>
